6095949a6899d247eb137d5c33ce8934c8eb1f52
M2tinho <151056872+M2tinho@users.noreply.github.com>
2024-05-20 21:51:01 +0200

    Update ligne_de_test.h

--- ligne_de_test.h
+++ ligne_de_test.h
@@ -15,10 +15,8 @@ else
     printf("Erreur lors de l'ajout de la valeur à ma colonne\n");
 
 // Ajoute d'autres valeurs à la colonne
-ajouter_valeur(ma_colonne, 25);
-ajouter_valeur(ma_colonne, 38);
-ajouter_valeur(ma_colonne, 38);
-ajouter_valeur(ma_colonne, 7);
+ajouter_valeur(ma_colonne, 10);
+ajouter_valeur(ma_colonne, 70);
 
 // Affiche toutes les valeurs de la colonne
 afficher_colonne(ma_colonne);
@@ -31,27 +29,23 @@ printf("Taille logique: %d\n", ma_colonne->nombre_elements);
 printf("Capacité physique: %d\n", ma_colonne->capacite);
 
 // Affiche le nombre d'occurrences d'une valeur spécifique dans la colonne
-printf("Le nombre d'occurrences de %d est %d\n", 38, compter_occurrences(ma_colonne, 38));
-printf("Le nombre d'occurrences de %d est %d\n", 7, compter_occurrences(ma_colonne, 7));
-printf("Le nombre d'occurrences de %d est %d\n", 3, compter_occurrences(ma_colonne, 3));
+printf("Le nombre d'occurrences de %d est %d\n", 20, compter_occurrences(ma_colonne, 20));
 
 // Affiche la valeur à un indice spécifique dans la colonne
-printf("La valeur à l'indice %d est %d\n", 1, rechercher_valeur(ma_colonne, 1));
 printf("La valeur à l'indice %d est %d\n", 3, rechercher_valeur(ma_colonne, 3));
-printf("La valeur à l'indice %d est %d\n", 10, rechercher_valeur(ma_colonne, 10));
+
 
 // Affiche le nombre de valeurs supérieures à une valeur spécifique dans la colonne
-printf("Le nombre de valeurs supérieures à %d est %d\n", 20, compter_valeurs_superieures(ma_colonne, 20));
-printf("Le nombre de valeurs supérieures à %d est %d\n", 35, compter_valeurs_superieures(ma_colonne, 35));
+printf("Le nombre de valeurs supérieures à %d est %d\n", 5, compter_valeurs_superieures(ma_colonne, 5));
+
 
 // Affiche le nombre de valeurs inférieures à une valeur spécifique dans la colonne
-printf("Le nombre de valeurs inférieures à %d est %d\n", 20, compter_valeurs_inferieures(ma_colonne, 20));
-printf("Le nombre de valeurs inférieures à %d est %d\n", 40, compter_valeurs_inferieures(ma_colonne, 40));
+printf("Le nombre de valeurs inférieures à %d est %d\n", 3, compter_valeurs_inferieures(ma_colonne, 3));
+
 
 // Affiche le nombre de valeurs égales à une valeur spécifique dans la colonne
-printf("Le nombre de valeurs égales à %d est %d\n", 20, compter_valeurs_egales(ma_colonne, 20));
-printf("Le nombre de valeurs égales à %d est %d\n", 10, compter_valeurs_egales(ma_colonne, 10));
-printf("Le nombre de valeurs égales à %d est %d\n", 38, compter_valeurs_egales(ma_colonne, 38));
+printf("Le nombre de valeurs égales à %d est %d\n", 7, compter_valeurs_egales(ma_colonne, 7));
+
 
 // Libère la mémoire allouée pour la colonne
 liberer_colonne(&ma_colonne);
